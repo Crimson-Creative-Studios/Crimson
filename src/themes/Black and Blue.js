e1f2dee900fcd3cc8fe9a177e69195b5db7d3adf
Lime <LimeDEV8756@gmail.com>
2023-07-04 21:54:12 +0100

    Fixes

--- src/themes/Black and Blue.js
+++ src/themes/Black and Blue.js
@@ -2,7 +2,7 @@ clearColor()
 
 document.getElementById("element").value = ""
 document.getElementById("backgroundmainchange").value = "#0F64AC"
-document.getElementById("backgroundaltchange").value = "#0a5391"
+document.getElementById("backgroundaltchange").value = "#0e5fa3"
 document.getElementById("textmainchange").value = "white"
 document.getElementById("textaltchange").value = "#c4c4c4"
 document.getElementById("buttonmainchange").value = "#000000"
